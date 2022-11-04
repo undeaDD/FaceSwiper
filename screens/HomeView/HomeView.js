ce92fb6f1e53449b240d6f31a1e076a23d6f51cf
Dominic Drees <dominic.drees@live.de>
2022-11-04 15:57:06 +0100

    nice image verison

--- screens/HomeView/HomeView.js
+++ screens/HomeView/HomeView.js
@@ -1,26 +1,40 @@
-import { FlatList, View, useWindowDimensions } from 'react-native';
+import { useRef, useEffect } from "react";
+import ViewShot from "react-native-view-shot";
+import { FlatList, useWindowDimensions, Image, Button } from 'react-native';
 
 export const HomeViewOptions = {
     title: "FaceSwiper",
 }
 
 const data = [
-    {id: "0", color: "red"},
-    {id: "1", color: "green"},
-    {id: "2", color: "blue"},
-    {id: "3", color: "orange"},
-    {id: "4", color: "gray"},
-    {id: "5", color: "black"},
-    {id: "6", color: "purple"},
-    {id: "7", color: "yellow"},
+    [
+        {id: "0", image: require("./../../assets/faces/11.jpg")},
+        {id: "1", image: require("./../../assets/faces/21.jpg")},
+        {id: "2", image: require("./../../assets/faces/31.jpg")},
+        {id: "3", image: require("./../../assets/faces/41.jpg")},
+        {id: "4", image: require("./../../assets/faces/51.jpg")},
+    ],
+    [
+        {id: "0", image: require("./../../assets/faces/12.jpg")},
+        {id: "1", image: require("./../../assets/faces/22.jpg")},
+        {id: "2", image: require("./../../assets/faces/32.jpg")},
+        {id: "3", image: require("./../../assets/faces/42.jpg")},
+        {id: "4", image: require("./../../assets/faces/52.jpg")},
+    ],
+    [
+        {id: "0", image: require("./../../assets/faces/13.jpg")},
+        {id: "1", image: require("./../../assets/faces/23.jpg")},
+        {id: "2", image: require("./../../assets/faces/33.jpg")},
+        {id: "3", image: require("./../../assets/faces/43.jpg")},
+        {id: "4", image: require("./../../assets/faces/53.jpg")},
+    ]
 ];
 
-const Swiper = ({items}) => {
+const Swiper = ({items, number}) => {
     const { width } = useWindowDimensions();
-
     return (
         <FlatList 
-            data={items}
+            data={items[number]}
             keyExtractor={item => item.id}
             horizontal={true}
             pagingEnabled={true}
@@ -39,17 +53,34 @@ const Swiper = ({items}) => {
 
 const renderItem = ({item}, width) => {
     return (
-        <View style={{width: width, height: "100%", backgroundColor: item.color}}></View>
+        <Image 
+            source={item.image}
+            style={{
+                width: width,
+                height: "100%",
+                backgroundColor: item.color,
+                resizeMode: "stretch"
+            }} 
+        />
     );
 }
 
-export default HomeView = () => {
-    
+export default HomeView = ({ navigation }) => {
+    const ref = useRef();
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <Button onPress={() => ref.current.capture()} title="Snap" />
+            ),
+        });
+    }, [navigation]);
+
     return (
-        <View style={{flex: 1}}>
-            <Swiper items={data} />
-            <Swiper items={data} />
-            <Swiper items={data} />
-        </View>
+        <ViewShot style={{flex: 1}} ref={ref} options={{ fileName: "Face", format: "jpg", quality: 0.9 }} >
+            <Swiper items={data} number={0} />
+            <Swiper items={data} number={1} />
+            <Swiper items={data} number={2} />
+        </ViewShot>
     );
 }
